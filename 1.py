c1a4272c731ec122cb9652e3cfb571f96c22483b
thinkter <ashmangamer0406@gmail.com>
2025-12-01 15:51:18 +0530

    day1 1st half

---
+++ 1.py
@@ -0,0 +1,27 @@
+from collections import deque
+with open("input1.txt", "r", encoding="utf-8") as f:
+    data = f.read()
+    ins = data.splitlines()
+    print(ins)
+    # shit starts at 50 
+    c = 0
+    i = 50 
+    d = deque(range(0, 100), maxlen=100)
+    d.rotate(50) #shit gotta start at fitty
+    #rotate( pos: right, neg: left)
+    for line in ins:
+        if line is not "":
+            #print(line[0])
+            dir = line[0]
+            num = int(line[1:])
+
+            if dir == "L":
+                d.rotate(-num)
+            elif dir == "R":
+                d.rotate(num)
+            if(d[0] == 0):
+                c = c+1
+print(d)
+print("\n\n")
+print(c) 
+
